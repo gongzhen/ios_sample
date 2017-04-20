d7bb7bbc93477ea0eb9e187c66597a8688dbd444
gongzhen <gongzhengz@gmail.com>
2017-04-20 14:11:48 -0700

    Added interface Tuple for Apple interview questions

--- objc/StringTupleStruct/AppleInterview/main.m
+++ objc/StringTupleStruct/AppleInterview/main.m
@@ -8,22 +8,23 @@
 
 #import <Foundation/Foundation.h>
 
-struct Tuple {
-    char* first;
-    char* second;
-};
-typedef struct Tuple Tuple;
+@interface Tuple : NSObject
+@property(copy, nonatomic) NSString *first;
+@property(copy, nonatomic) NSString *second;
 
-Tuple TupleMake(char *first, char *second) {
-    Tuple tuple;
-    // tuple.first = first;
-    // tuple.second = second;
-    tuple.first = strdup(first);
-    tuple.second = strdup(second);
-//    strcpy(tuple.first, first); // bad access
-//    strcpy(tuple.second, second);
-    return tuple;
+@end
+
+@implementation Tuple
+
+- (instancetype)initWithFirst:(NSString *)first second:(NSString *)second {
+    if(self = [super init]) {
+        _first = [first copy];
+        _second = [second copy];
+    }
+    return self;
 }
+
+@end
 
 @interface Solution : NSObject
 
@@ -35,13 +36,11 @@ Tuple TupleMake(char *first, char *second) {
 
 -(NSMutableArray *)findCouples:(NSArray *)couples guest:(NSSet *)guest {
     NSMutableArray *result = [[NSMutableArray alloc] init];
-    for(NSValue *value in couples) {
-        Tuple tuple;
-        [value getValue:&tuple];
-        NSString *first = [NSString stringWithUTF8String:tuple.first];
-        NSString *second = [NSString stringWithUTF8String:tuple.second];
-        if([guest containsObject:first] && [guest containsObject:second]) {
-            [result addObject:[NSValue valueWithBytes:&tuple objCType:@encode(Tuple)]];
+    for(id obj in couples) {
+        if([obj isKindOfClass:[Tuple class]] == YES) {
+            if([guest containsObject:((Tuple *)obj).first] == YES && [guest containsObject:((Tuple *)obj).second] == YES) {
+                [result addObject:obj];
+            }
         }
     }
     return result;
@@ -53,19 +52,17 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         NSLog(@"Hello, World!");
-        Tuple tuple1 = TupleMake("Chris", "Haha");
-        Tuple tuple2 = TupleMake("John", "Kassie");
-        NSMutableArray *couples = [[NSMutableArray alloc] initWithObjects: [NSValue valueWithBytes:&tuple1 objCType:@encode(Tuple)], [NSValue valueWithBytes:&tuple2 objCType:@encode(Tuple)], nil];
+        Tuple* tuple1 = [[Tuple alloc] initWithFirst:@"Chris" second:@"Haha"];
+        Tuple* tuple2 = [[Tuple alloc] initWithFirst:@"John" second:@"Kassie"];
+        NSArray *couples = [[NSArray alloc] initWithObjects: tuple1, tuple2,nil];
         NSSet *guest = [[NSSet alloc] initWithObjects:@"Chris", @"Haha", @"John", @"Lucy", nil];
         Solution *obj = [[Solution alloc] init];
         NSMutableArray *result = [obj findCouples:couples guest:guest];
-        for(NSValue *value in result) {
-            Tuple tuple;
-            [value getValue:&tuple];
-            NSString *first = [NSString stringWithUTF8String:tuple.first];
-            NSString *second = [NSString stringWithUTF8String:tuple.second];
-            DLog(@"result:%@", first);
-            DLog(@"result:%@", second);
+        for(id obj in result) {
+            if([obj isKindOfClass:[Tuple class]] == YES) {
+                DLog(@"%@", ((Tuple *)obj).first);
+                DLog(@"%@", ((Tuple *)obj).second);
+            }
         }
     }
     return 0;
