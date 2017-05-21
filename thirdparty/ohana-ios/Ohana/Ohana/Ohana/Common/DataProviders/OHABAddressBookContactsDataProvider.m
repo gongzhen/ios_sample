558bec78a8606320faed5c1a1b75bf3cc2056a9a
gongzhen <gongzhengz@gmail.com>
2017-05-21 14:40:17 -0700

    Ohana 8.1 version

---
+++ thirdparty/ohana-ios/Ohana/Ohana/Ohana/Common/DataProviders/OHABAddressBookContactsDataProvider.m
@@ -0,0 +1,13 @@
+//
+//  OHABAddressBookContactsDataProvider.m
+//  Ohana
+//
+//  Created by zhen gong on 5/21/17.
+//  Copyright © 2017 zhen gong. All rights reserved.
+//
+
+#import "OHABAddressBookContactsDataProvider.h"
+
+@implementation OHABAddressBookContactsDataProvider
+
+@end
