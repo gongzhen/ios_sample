e0773cc81e14bb0ee083894e43705e529406ee93
gongzhen <gongzhengz@gmail.com>
2017-06-21 13:48:01 -0700

    Added myView

--- uikit/animation/iOS10DrawShapesTutorial/iOS10DrawShapesTutorial/ShapeView.swift
+++ uikit/animation/iOS10DrawShapesTutorial/iOS10DrawShapesTutorial/ShapeView.swift
@@ -47,7 +47,24 @@ class ShapeView: UIView {
     }
     
     func drawRectangle() {
-    
+        let center = CGPoint(x: self.frame.size.width / 2.0, y: self.frame.size.height / 2.0)
+        let rectangleWidth:CGFloat = 100.0
+        let rectangleHeight:CGFloat = 100.0
+        
+        guard let ctx = UIGraphicsGetCurrentContext() else { return }
+        
+        //4
+        ctx.addRect(CGRect(x: center.x - (0.5 * rectangleWidth), y: center.y - (0.5 * rectangleHeight), width: rectangleWidth, height: rectangleHeight))
+        ctx.setLineWidth(10)
+        ctx.setStrokeColor(UIColor.gray.cgColor)
+        ctx.strokePath()
+        
+        //5
+        ctx.setFillColor(UIColor.green.cgColor)
+        
+        ctx.addRect(CGRect(x: center.x - (0.5 * rectangleWidth), y: center.y - (0.5 * rectangleHeight), width: rectangleWidth, height: rectangleHeight))
+        
+        ctx.fillPath()
     }
     
     func drawCircle() {
--- uikit/animation/iOS10DrawShapesTutorial/iOS10DrawShapesTutorial/ViewController.swift
+++ uikit/animation/iOS10DrawShapesTutorial/iOS10DrawShapesTutorial/ViewController.swift
@@ -16,6 +16,8 @@ class ViewController: UIViewController {
     
     var button3:UIButton?
     
+    var myView:UIView?
+    
     private func createButton(title:String, color:UIColor, tag:Int) -> UIButton {
         let button = UIButton(frame: CGRect.zero)
         button.titleLabel?.text = title
@@ -30,42 +32,71 @@ class ViewController: UIViewController {
         return button
     }
     
+    private func createView() -> UIView {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = UIColor.cyan
+        return view
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         button1 = createButton(title: "Rectangle", color: UIColor.black, tag: 1)
         button2 = createButton(title: "Lines", color: UIColor.black, tag: 2)
         button3 = createButton(title: "Circle", color: UIColor.black, tag: 3)
-        view.backgroundColor = UIColor.blue
+        myView = createView()
+        view.backgroundColor = UIColor.white
+        
         view.addSubview(button1!)
         view.addSubview(button2!)
         view.addSubview(button3!)
-        let viewsDict:[String: UIView] = ["button1":button1!, "button2":button2!, "button3":button3!]
+        view.addSubview(myView!)
         
-        let descHorizontal = "H:|-[button1(>=100)]-20-[button2(==button1)]-20-[button3(==button1)]-|"
-        let descVertical1 = "V:|-100-[button1(40)]"
-        let descVertical2 = "V:|-100-[button2(==button1)]"
-        let descVertical3 = "V:|-100-[button3(==button1)]"
+        let horizontalSpacing = 20
+        let verticalSpacing = 100
+        let viewVerticalSpace = 10
+        let cornerMargin = 10
+        let viewsDict:[String: UIView] = ["button1":button1!, "button2":button2!, "button3":button3!, "myView":myView!]
+        let metrics = ["horizontalSpacing" : horizontalSpacing, "cornerMargin": cornerMargin, "verticalSpacing":verticalSpacing, "viewVerticalSpace":viewVerticalSpace]
+        let descHorizontal = "H:|-(==cornerMargin)-[button1(>=100)]-horizontalSpacing-[button2(==button1)]-horizontalSpacing-[button3(==button1)]-(==cornerMargin)-|"
+        let descVertical1 = "V:|-(==verticalSpacing)-[button1]"
+        let descVertical2 = "V:|-(==verticalSpacing)-[button2]"
+        let descVertical3 = "V:|-(==verticalSpacing)-[button3]"
         let horizontalConstraints = NSLayoutConstraint.constraints(withVisualFormat: descHorizontal,
-                                                                   options: NSLayoutFormatOptions(rawValue: 0),
-                                                                   metrics: nil,
+                                                                   options: .alignAllCenterY,
+                                                                   metrics: metrics,
                                                                    views: viewsDict)
         let verticalConstraints1 = NSLayoutConstraint.constraints(withVisualFormat: descVertical1,
-                                                                 options: NSLayoutFormatOptions(rawValue: 0),
-                                                                 metrics: nil,
+                                                                 options: .alignAllCenterY,
+                                                                 metrics: metrics,
                                                                  views: viewsDict)
         let verticalConstraints2 = NSLayoutConstraint.constraints(withVisualFormat: descVertical2,
                                                                  options: NSLayoutFormatOptions(rawValue: 0),
-                                                                 metrics: nil,
+                                                                 metrics: metrics,
                                                                  views: viewsDict)
         let verticalConstraints3 = NSLayoutConstraint.constraints(withVisualFormat: descVertical3,
                                                                  options: NSLayoutFormatOptions(rawValue: 0),
-                                                                 metrics: nil,
+                                                                 metrics: metrics,
                                                                  views: viewsDict)
+        
+        let viewHorizontal = "H:|-[myView]-|"
+        let viewVeritical = "V:[button1]-(==viewVerticalSpace)-[myView]-(==viewVerticalSpace)-|"
+        let viewHorizontalConstraint = NSLayoutConstraint.constraints(withVisualFormat: viewHorizontal,
+                                                                      options: NSLayoutFormatOptions(rawValue:0),
+                                                                      metrics: metrics,
+                                                                      views: viewsDict)
+        
+        let viewVerticalConstraint = NSLayoutConstraint.constraints(withVisualFormat: viewVeritical,
+                                                                      options: NSLayoutFormatOptions(rawValue:0),
+                                                                      metrics: metrics,
+                                                                      views: viewsDict)
         self.view.addConstraints(horizontalConstraints)
         self.view.addConstraints(verticalConstraints1)
         self.view.addConstraints(verticalConstraints2)
         self.view.addConstraints(verticalConstraints3)
+        self.view.addConstraints(viewHorizontalConstraint)
+        self.view.addConstraints(viewVerticalConstraint)
     }
 
     override func didReceiveMemoryWarning() {
@@ -75,6 +106,9 @@ class ViewController: UIViewController {
     
     func buttonPressed(_ sender: AnyObject) {
         
+        let myView = ShapeView(frame:CGRect(x: 25, y: 200, width: 280, height: 250) , shape: sender.tag)
+        myView.backgroundColor = UIColor.cyan
+        view.addSubview(myView)
     }
 
 
