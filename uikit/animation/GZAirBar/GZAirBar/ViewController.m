5383228a0f36bfc9fbbaf5464c5c65f0235f1ba4
gong zhen <gongzhengz@gmail.com>
2018-10-28 14:54:56 -0700

    Reactive Cocoa Framework

--- uikit/animation/GZAirBar/GZAirBar/ViewController.m
+++ uikit/animation/GZAirBar/GZAirBar/ViewController.m
@@ -8,15 +8,63 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+static double const normalStateHeight = 128.0;
+static double const compactStateHeight = 64;
+static double const expandedStateHeight = 284;
 
+@interface ViewController () <UITableViewDataSource, UITableViewDelegate>
+
+@property (strong, nonatomic) UITableView* firstTableView;
+@property (strong, nonatomic) UITableView* secondTableView;
+@property (strong, nonatomic) UIView* airBar;
+@property (strong, nonatomic) UIView* backgroundView;
+
+
+//fileprivate var airBar: UIView!
+//fileprivate var backgroundView: UIView!
+//fileprivate var darkMenuView: MenuView!
+//fileprivate var lightMenuView: MenuView!
+//fileprivate var normalView: NormalView!
+//fileprivate var expandedView: UIView!
+//fileprivate var backButton: UIButton!
+//fileprivate var barController: BarController!
 @end
 
 @implementation ViewController
 
+#pragma mark - Properties
+
+-(UITableView *)firstTableView {
+    if(_firstTableView == nil) {
+        _firstTableView = [[UITableView alloc] initWithFrame:CGRectZero style:UITableViewStylePlain];
+        _firstTableView.backgroundColor = [UIColor colorWithRed:0.8 green:0.9 blue:0.9 alpha:1];
+        _firstTableView.rowHeight = 80.f;
+        _firstTableView.dataSource = self;
+    }
+    return _firstTableView;
+}
+
+-(UITableView *)secondTableView {
+    if(_secondTableView == nil) {
+        _secondTableView = [[UITableView alloc] initWithFrame:CGRectZero style:UITableViewStylePlain];
+        _secondTableView.backgroundColor = [UIColor colorWithRed:0.8 green:0.9 blue:0.9 alpha:1];
+        _secondTableView.rowHeight = 80.f;
+        _secondTableView.dataSource = self;
+    }
+    return _secondTableView;
+}
+
+-(UIView *)backgroundView {
+    if(_backgroundView == nil) {
+        _backgroundView = [[UIView alloc] initWithFrame:CGRectZero];
+    }
+    return _backgroundView;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self.view insertSubview:self.firstTableView atIndex:0];
+    [self.view insertSubview:self.secondTableView atIndex:0];
 }
 
 
